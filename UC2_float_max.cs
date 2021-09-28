863a840198a5588261a3f694ec4b49deb8e2d2b3
kavitaparsewar <parsewarkavita@gmail.com>
2021-09-28 23:12:40 +0530

    [Kavita] UC1 find float maximum number added

---
+++ UC2_float_max.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day13_Generics_Assignment
+{
+    class UC2_float_max
+    {
+        public static double MaximumFloatNumber(double firstValue, double secondValue, double thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("firstNumber,secondNumber and thirdNumber are same");
+        }
+    }
+}
