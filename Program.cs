e91c082f6ae3d52f2e32eeac2349a194ac132c49
kavitaparsewar <parsewarkavita@gmail.com>
2021-09-28 23:22:53 +0530

    [Kavita] main method i.e program.cs added

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Day13_Generics_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" maximum Number from given integer");
+            int output = UC1_int_max.MaximumIntegerNumber(50,60,70);
+            Console.WriteLine(output);
+
+            Console.WriteLine(" maximum Number from given float values");
+            double doubleoutput = UC2_float_max.MaximumFloatNumber(121.5,67.5,55.5);
+            Console.WriteLine(doubleoutput);
+
+            Console.WriteLine(" maximum Number from given string");
+            string strigoutput = UC3_string_max.MaximumStringNumber("38", "67", "99");
+            Console.WriteLine(strigoutput);
+
+        }
+    }
+}
